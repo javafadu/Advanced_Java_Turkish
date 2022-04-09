e5d42881647be9aeba07dac0a2beb2f8344b6581
javafadu <javafadu@gmail.com>
2022-04-10 01:15:52 +0300

    8 apr

---
+++ src/JavaProjectsFaDu/P05_HalukMarketOOP/OnlineGreenGrocer.java
@@ -0,0 +1,16 @@
+package JavaProjectsFaDu.P05_HalukMarketOOP;
+
+
+public class OnlineGreenGrocer {
+
+    public static void main(String[] args) {
+
+        System.out.println("******* WELCOME TO ONLINE GREENGROCER *******");
+
+        Products prd1 = new Products("Apple","05",1.20);
+
+
+
+
+    }
+}
---
+++ src/JavaProjectsFaDu/P05_HalukMarketOOP/Products.java
@@ -0,0 +1,19 @@
+package JavaProjectsFaDu.P05_HalukMarketOOP;
+
+public class Products {
+
+    String name;
+    String code;
+    double price;
+    int count;
+
+    public Products () {
+
+    }
+
+    public Products(String name, String code, double price) {
+        this.name = name;
+        this.code = code;
+        this.price = price;
+    }
+}
