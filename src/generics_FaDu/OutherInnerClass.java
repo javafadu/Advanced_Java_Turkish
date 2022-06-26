3683a902797bae1b007348d3703e3a98a013d7b2
javafadu <javafadu@gmail.com>
2022-06-26 11:38:21 +0300

    26 Jun 2022

--- src/generics_FaDu/OutherInnerClass.java
+++ src/generics_FaDu/OutherInnerClass.java
@@ -1,13 +1,18 @@
 package generics_FaDu;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class OutherInnerClass { // Outher
 
-    public class Inner1 { // Inner Class
+    String str0 = "Main Class";
 
+    public class Inner1 { // Inner Class
+        String str1 = "Inner Class1";
     }
 
     public class Inner2 { // Inner Class
 
+        String str2 = "Inner Class2";
     }
 
 
