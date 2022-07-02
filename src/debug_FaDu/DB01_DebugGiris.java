b99dde8427bd94a54a2d2ee426d56d781c9fdf97
javafadu <javafadu@gmail.com>
2022-07-02 18:37:13 +0300

    02 Jul 2022 - 1900

---
+++ src/debug_FaDu/DB01_DebugGiris.java
@@ -0,0 +1,18 @@
+package debug_FaDu;
+
+/*
+Debug yapiyorsan, kodu anlamadin demektir.
+Debug yaparken kodu anlamiyorsan, nasil debug yapilir, onu anlamadin demektir.
+Debug yaparken kodu anladigini dusunuyorsan, aslinda debuga ihtiyacin olmadigini anladin demektir.
+Debug etmek kodun mental bir modelinin eksikligine isarettir..
+
+
+İki durumda debugger gerekli olabilir:
+ *  Mevcut kod devralındığında.
+ *  Yeni kod yazıldığında.
+her iki durumda da debugger çalıştırmamaya dikkat edilmeli.
+Amac sıfır debug aktivitesi ile uygulamayı istenilen seviyeye getirebilmek.
+ */
+
+public class DB01_DebugGiris {
+}
